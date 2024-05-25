9082c0dec512b200d40448f4cc5c0695da8805a5
MiguelArr <miguelarroyocasades@gmail.com>
2024-05-25 16:51:10 +0200

    fixed stuff

--- sparkles.js
+++ sparkles.js
@@ -4,7 +4,7 @@ function trail(e) {
   
     // Generate random offsets
     var offsetX = Math.random() * 30 - 100; // Random number between -10 and 10
-    var offsetY = Math.random() * 30 - 40; // Random number between -10 and 10
+    var offsetY = Math.random() * 30 - 70; // Random number between -10 and 10
 
     Object.assign(div.style, {
         top: (e.pageY + offsetY) + 'px',
